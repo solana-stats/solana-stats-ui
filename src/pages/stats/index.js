f48b6579ef9493569e631c7fdfcb3c47f706a505
cryptogosu <github@cryptogosu.com>
2021-06-07 23:53:11 -0500

    Updated api calls

--- src/pages/stats/index.js
+++ src/pages/stats/index.js
@@ -14,12 +14,12 @@ export default function Stats() {
 
     useEffect(() => {
         if (!data) {
-            const tps = fetchData('http://localhost:8081/stats-bff/tps');
-            const pastDayTransactions = fetchData('http://localhost:8081/stats-bff/transactions/transaction?interval=H');
-            const pastDayFees = fetchData('http://localhost:8081/stats-bff/fee');
-            const numSerum = fetchData('http://localhost:8081/stats-bff/transactions/serum?interval=H');
-            const fees = fetchData('http://localhost:8081/stats-bff/transactions/fee?interval=H');
-            const numTransactions = fetchData('http://localhost:8081/stats-bff/transactions/transaction?interval=M');
+            const tps = fetchData('https://api-solanastats.com/stats-bff/tps');
+            const pastDayTransactions = fetchData('https://api-solanastats.com/stats-bff/transactions/transaction?interval=H');
+            const pastDayFees = fetchData('https://api-solanastats.com/stats-bff/fee');
+            const numSerum = fetchData('https://api-solanastats.com/stats-bff/transactions/serum?interval=H');
+            const fees = fetchData('https://api-solanastats.com/stats-bff/transactions/fee?interval=H');
+            const numTransactions = fetchData('https://api-solanastats.com/stats-bff/transactions/transaction?interval=M');
 
             Promise.all([tps, pastDayTransactions, pastDayFees, numSerum, fees, numTransactions]).then((results) => {
                 console.log(results);
