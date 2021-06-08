4b728d9b076f9887e0005c517142753b7ef0e19c
cryptogosu <github@cryptogosu.com>
2021-06-07 23:51:44 -0500

    Added first stats

--- src/pages/stats/index.js
+++ src/pages/stats/index.js
@@ -1,18 +1,19 @@
 import React, {useEffect, useState} from 'react';
+import StatsChart from './../../components/statsChart';
 import './stats.css'
 
 const fetchData = async (url) => {
     const response = await fetch(url);
     const json = await response.json();
-    return json.result;
+    return json;
 }
 
 export default function Stats() {
 
-    const [chartsFetched, setChartsFetched] = useState(false);
+    const [data, setData] = useState(false);
 
     useEffect(() => {
-        if (!chartsFetched) {
+        if (!data) {
             const tps = fetchData('http://localhost:8081/stats-bff/tps');
             const pastDayTransactions = fetchData('http://localhost:8081/stats-bff/transactions/transaction?interval=H');
             const pastDayFees = fetchData('http://localhost:8081/stats-bff/fee');
@@ -20,16 +21,20 @@ export default function Stats() {
             const fees = fetchData('http://localhost:8081/stats-bff/transactions/fee?interval=H');
             const numTransactions = fetchData('http://localhost:8081/stats-bff/transactions/transaction?interval=M');
 
-            Promise.all([tps, pastDayTransactions, pastDayFees, numSerum, fees, numTransactions]).then((values) => {
-                console.log(values);
-                setChartsFetched(values);
+            Promise.all([tps, pastDayTransactions, pastDayFees, numSerum, fees, numTransactions]).then((results) => {
+                console.log(results);
+                setData(results);
+                console.log(data);
             })
         }
-    }, [chartsFetched]);
+    }, [data]);
 
     return (
         <div className={'view'}>
-
+            {data[1] ? <StatsChart title={'24hr Transactions'} data={data[1]}/> : <div/> }
+            {data[3] ? <StatsChart title={'24hr Serum Transactions'} data={data[3]}/> : <div/> }
+            {data[4] ? <StatsChart title={'24hr Fees'} data={data[4]}/> : <div/> }
+            {data[5] ? <StatsChart title={'Recent Transactions'} data={data[5]}/> : <div/> }
         </div>
     )
 }
